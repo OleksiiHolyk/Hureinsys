90974c57f2ea9e5c8419185ec243a0f18df376ba
Oleksii <a_golik@hotmail.com>
2016-09-15 12:38:32 +0300

    1. change link (remove host address) from getUser and addUser 2. change ajax request param (data, dataType, contentType)

--- src/main/resources/static/POST/addUser.js
+++ src/main/resources/static/POST/addUser.js
@@ -2,16 +2,14 @@
  * Created by Oleksii on 13.09.2016.
  */
 $(document).ready(function () {
-
-    $('#clickMe').click(function () {
-        alert("Luke, i'm your father");
-        $.ajax({
-            method: "POST",
-            url: "http://localhost:8080/users",
-            data: {
-                "username": "john",
-                "email": "lennon"
-            }
-        });
+    $.ajax({
+        url: "/users",
+        dataType: "JSON",
+        type: "POST",
+        contentType: "application/json",
+        data: JSON.stringify({
+            "username": "john",
+            "email": "lennon"
+        })
     });
 });
