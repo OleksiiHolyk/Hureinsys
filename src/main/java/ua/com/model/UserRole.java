de8784d2e50fc9d0b83338a89b21c5973d65801c
Oleksii <a_golik@hotmail.com>
2016-09-23 12:37:00 +0300

    change roles enum

--- src/main/java/ua/com/model/UserRole.java
+++ src/main/java/ua/com/model/UserRole.java
@@ -4,6 +4,6 @@ package ua.com.model;
  * Created by Oleksii on 10.05.2016.
  * oleksii.holyk@outlook.com
  */
-public enum UserRole {
-    ROLE_ADMIN, ROLE_USER, ADMIN
+enum UserRole {
+    ROLE_ADMIN, ROLE_USER
 }
