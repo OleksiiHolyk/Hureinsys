18c1822e8a0498ab58e90f73ff0cc475822759df
Oleksii <a_golik@hotmail.com>
2016-09-25 01:11:48 +0300

    change work getUsers btn (result and clear form fileds)

--- src/main/resources/static/js/getUser.js
+++ src/main/resources/static/js/getUser.js
@@ -8,11 +8,11 @@ function getUserById() {
         method: "GET",
         url: "/users/" + $('#userIdInput').val()
     }).done(function (data) {
-        $('#userData').show();
-        $('#phoneNumber .val').append(data.phoneNumber);
-        $('#username .val').append(data.username);
-        $('#email .val').append(data.email);
-        $('#password .val').append(data.password);
+        $('#userById').show();
+        $('#phoneNumber').find('.val').append(data.phoneNumber);
+        $('#username').find('.val').append(data.username);
+        $('#email').find('.val').append(data.email);
+        $('#password').find('.val').append(data.password);
     }).fail(function (jqXHR, textStatus) {
         alert("Request failed: " + textStatus);
     });
@@ -23,38 +23,46 @@ function getUsers() {
         method: "GET",
         url: "/users"
     }).done(function (data) {
-        console.log(data);
-        console.log(data._embedded.users);
-
+        $('#usersList').show();
         var user = data._embedded.users;
-        var usersCount = data._embedded.users.length;
-        $('#userData').show();
-
-
         $.each(user, function (key, value) {
             pasteUsers(user[key]);
         });
-
-
     }).fail(function (jqXHR, textStatus) {
         alert("Request failed: " + textStatus);
     });
 }
 
 function pasteUsers(user) {
-    $('#phoneNumber .val').append(user.phoneNumber);
-    $('#username .val').append(user.username);
-    $('#email .val').append(user.email);
-    $('#password .val').append(user.password);
+    var r1, r2, r3, r4;
+    r1 = $('<div></div>').addClass('phoneNumber').append().append(user.phoneNumber);
+    r2 = $('<div></div>').addClass('username').append(user.username);
+    r3 = $('<div></div>').addClass('email').append(user.email);
+    r4 = $('<div></div>').addClass('password').append(user.password);
+    $('<div></div>').addClass('row').append(r1).append(r2).append(r3).append(r4).appendTo(document.getElementById('usersList'));
 }
 
+function clearFormFields() {
+    $('#phoneNumber').find('.val').html('');
+    $('#username').find('.val').html('');
+    $('#email').find('.val').html('');
+    $('#password').find('.val').html('');
+    $('#usersList').html('');
+}
 
 $(document).ready(function () {
     $("#butGetUserById").click(function () {
+        if($('#userIdInput').val()===''){
+            alert("field with ID must not be empty")
+        } else
+        clearFormFields();
         getUserById();
     });
 
     $("#butGetUsers").click(function () {
+        $('#userIdInput').val('');
+        $('#userById').hide();
+        clearFormFields();
         getUsers();
     });
 });
